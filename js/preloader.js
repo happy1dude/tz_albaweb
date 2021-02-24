4cf0f5d026d2bd169ed9f28bf46196d969ee4db7
happy1dude <63054916+happy1dude@users.noreply.github.com>
2021-02-24 20:17:39 +0700

    Update preloader.js

--- js/preloader.js
+++ js/preloader.js
@@ -1,8 +1,17 @@
-window.onload = () => {
+window.addEventListener('load', () => {
     setTimeout(() => {
         let preloader = document.getElementById('preloader-page');
         if (!preloader.classList.contains('done') ) {
             preloader.classList.add('done')
         }
     }, 1000)
-}
+});
+
+// window.onload = () => {
+//     setTimeout(() => {
+//         let preloader = document.getElementById('preloader-page');
+//         if (!preloader.classList.contains('done') ) {
+//             preloader.classList.add('done')
+//         }
+//     }, 1000)
+// };
